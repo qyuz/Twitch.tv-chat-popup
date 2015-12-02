6285b07deccde0e06008975e86c297b2d70dec3e
Aleksejs Borisjuks <aleksejs.borisjuks@tele2.com>
2015-12-02 17:00:58 +0200

    opens popup window

--- util.js
+++ util.js
@@ -1,1 +1,8 @@
 window.POPUP_WIDTH_PERC = 25;
+
+function openWindow(url, screenWidthPercentage) {
+	var a = window,
+	b = document,
+	c = encodeURIComponent,
+	d = a.open(url + '/chat?popout=', 'bkmk_popup', 'left=' + ((a.screenX || a.screenLeft) + 10) + ',top=' + ((a.screenY || a.screenTop) + 10) + ',height=' + screen.height + 'px,width=' + screen.width / 100 * screenWidthPercentage + 'px,resizable=1,alwaysRaised=1');
+}
