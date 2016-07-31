fe60281e5591c487abb12ef48a4ab52b62f43683
qyuz <qyuz555@gmail.com>
2016-07-31 16:28:16 +0300

    extension working for https

--- util.js
+++ util.js
@@ -4,5 +4,5 @@ function openWindow(url, screenWidthPercentage) {
 	var a = window,
 	b = document,
 	c = encodeURIComponent,
-	d = a.open(url + '/chat?popout=', 'bkmk_popup', 'left=' + ((a.screenX || a.screenLeft) + 10) + ',top=' + ((a.screenY || a.screenTop) + 10) + ',height=' + screen.height + 'px,width=' + screen.width / 100 * screenWidthPercentage + 'px,resizable=1,alwaysRaised=1');
+	d = a.open(url + '/chat?popout=', 'bkmk_popup', 'left=0,top=0,height=985px,width=336px,resizable=1,alwaysRaised=1');
 }
